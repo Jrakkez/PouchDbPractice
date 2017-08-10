ca64e04ef7d0a74a15c89e831f39e7b5419312d4
Sam Martinez <samanatha-martinez@uiiowa.edu>
2017-08-10 17:29:29 -0500

    Supports put functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 //Sample PouchDb web application.
 
-var db = new PouchDB('rsvpouch');
 PouchDB.debug.enable('*');
 
+var db = new PouchDB('rsvpouch');
+var entryList = [];
+var docs = [];
+
 db.info().then(function (info) {
+    docs = info.doc_count > 0 ? fillDocs() : [];
     console.log(info);
+    console.log("Docs: " + docs);
 });
 
 var pouchy = new Vue({
@@ -13,19 +18,62 @@ var pouchy = new Vue({
         header: "RSVPouch",
         whatToDo: "Create a list of RSVP's!",
         name: '',
-        show: db.info().doc_count > 0
+        partySize: '',
+        fullName: "Full Name",
+        numInParty: "In Party:",
+        dateRsvpd: "Date RSVP'd"
     },
     computed: {
-        fullName: "", //todo
-        numInParty: "",
-        going: ""
+        entries: {
+            get: getEntries,
+            set: function (newValue) { this.entries = newValue; }
+        }
     },
     methods: {
         addToDb: function () {
-            var content = { "_id": this.data.name.value };
+            var content = {
+                _id : new Date().toISOString(),
+                name: this.name.valueOf(),
+                numInParty: this.partySize.valueOf()
+            };
 
-            db.put(content);
+            console.log("Entered name: " + content.name);
+            console.log("Entered party size: " + content.numInParty);
+
+            dbPut(content);
         }
     }
 });
 
+function fillDocs() {
+    db.allDocs({
+        include_docs: true,
+        attachments: true
+    }).then(function (result) {
+        docs = result.rows;
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function getEntries() {
+    docs.forEach(function (doc) {
+        entryList.push({
+            fullName: doc.fullName,
+            numInParty: doc.numInParty,
+            dateRsvpd: doc._id.toLocaleString()
+        })
+    });
+    return entryList;
+}
+
+function dbPut(content) {
+    db.put(content).then(function (response) {
+        docs.push(content);
+        pouchy.name = '';
+        pouchy.partySize = '';
+        console.log("dbPut: Success for " + content._id + "!")
+    }).catch(function (error) {
+        console.log("Put for " + content._id + " FAILED. \n Reason: " + error);
+    });
+}
